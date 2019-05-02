0730110b4f769c664685f6975bf268a3ed010ffd
talent_mbp2018 <hl010738@126.com>
2019-05-02 18:33:48 +0800

    当一个接口在初始化时并要求其父接口也完成初始化.     当一个接口在初始化时并要求其父接口也完成初始化. 只有在真正使用父接口时(如引用接口所定义的常量时)，才会进行初始化.

--- src/main/java/classloader/Test5.java
+++ src/main/java/classloader/Test5.java
@@ -1,5 +1,7 @@
 package classloader;
 
+import javax.swing.*;
+
 /*
     当一个接口在初始化时并要求其父接口也完成初始化.
     当一个接口在初始化时并要求其父接口也完成初始化. 只有在真正使用父接口时(如引用接口所定义的常量时)，才会进行初始化.
@@ -10,10 +12,30 @@ public class Test5 {
     }
 }
 
-interface Parent5{
-    public static int a = 5;
+interface Grandpa5{
+    public static Thread thread = new Thread(){
+        {
+        System.out.println("Grandpa5");
+        }
+    };
 }
 
-interface Child5 extends Parent5{
+interface Parent5 extends Grandpa5{
+    public static Thread thread = new Thread(){
+        {
+            System.out.println("Parent5");
+        }
+    };
+}
+
+class Grandson5 implements Parent5{
+    public static Thread thread = new Thread(){
+        {
+            System.out.println("GrandSon5");
+        }
+    };
+}
+
+class Child5 extends Grandson5 implements Parent5{
     public static int b = 6;
 }
