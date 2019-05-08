c1ae55408b8382e1c83a46c15f752709d00f7b84
talent_mbp2018 <hl010738@126.com>
2019-05-08 19:51:25 +0800

    自定义类加载器 类加载器的命名空间 类里面引用了其他类

--- src/main/java/classloader/Sample1.java
+++ src/main/java/classloader/Sample1.java
@@ -3,5 +3,7 @@ package classloader;
 public class Sample1 {
     public Sample1() {
         System.out.println("Sample1 loaded by: " + this.getClass().getClassLoader());
+
+        //System.out.println("From Sample1: " + Sample2.class);
     }
 }
--- src/main/java/classloader/Sample2.java
+++ src/main/java/classloader/Sample2.java
@@ -4,5 +4,6 @@ public class Sample2 {
     public Sample2() {
         System.out.println("Sample2 loaded by: " + this.getClass().getClassLoader());
         new Sample1();
+        System.out.println("From Sample2: " + Sample1.class);
     }
 }
