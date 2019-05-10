a0a81786aa09903ea629f4d4f598e4155d1932ef
talent_mbp2018 <hl010738@126.com>
2019-05-10 18:03:23 +0800

    类加载器 反射

---
+++ src/main/java/classloader/Test20.java
@@ -0,0 +1,33 @@
+package classloader;
+
+import java.lang.reflect.Method;
+
+/*
+    类加载 反射
+ */
+public class Test20 {
+    public static void main(String[] args) throws Exception{
+        MyClassLoader loader1 = new MyClassLoader("loader1");
+        MyClassLoader loader2 = new MyClassLoader("loader2");
+
+        Class<?> class1 = loader1.loadClass("classloader.Person");
+        Class<?> class2 = loader2.loadClass("classloader.Person");
+
+        // 两个加载器都会委托appclassloader 加载
+        System.out.println(class1 == class2); //true
+
+        Object obj1 = class1.newInstance();
+        Object obj2 = class2.newInstance();
+
+        Method method = class1.getMethod("setPerson", Object.class);
+
+        method.invoke(obj1, obj2);
+    }
+}
+
+class Person{
+    private Person person;
+    public void setPerson(Object object){
+        this.person = (Person) object;
+    }
+}
