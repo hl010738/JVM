c71241d65d9f4310a21c932194c13cee758254ff
talent_mbp2018 <hl010738@126.com>
2019-05-28 18:51:48 +0800

    类加载器，线程上下文加载器

--- src/main/java/classloader/Test27.java
+++ src/main/java/classloader/Test27.java
@@ -2,9 +2,7 @@ package classloader;
 
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
-import java.util.Enumeration;
 
 public class Test27 {
     public static void main(String[] args) throws Exception {
