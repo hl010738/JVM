663469e10cfedbcd73b76889b8c2c48267b9f9f7
talent_mbp2018 <hl010738@126.com>
2019-05-28 18:09:57 +0800

    类加载器，线程上下文加载器

--- src/main/java/classloader/Test27.java
+++ src/main/java/classloader/Test27.java
@@ -9,12 +9,7 @@ import java.util.Enumeration;
 public class Test27 {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        //Connection connection = DriverManager.getDrivers();
-        Connection connection = null;
-        System.out.println(connection.getClass().getClassLoader());
-        Enumeration<Driver> drivers = DriverManager.getDrivers();
-        while (drivers.hasMoreElements()){
-            System.out.println(drivers.nextElement().getClass().getClassLoader());
-        }
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root", "root");
+
     }
 }
