08df0930d5e68dc7bc4bfa1b3a8647f084028206
talent_mbp2018 <hl010738@126.com>
2019-06-01 19:17:24 +0800

    方法重载的字节码分析

--- src/main/java/bytecode/Test5.java
+++ src/main/java/bytecode/Test5.java
@@ -1,6 +1,17 @@
 package bytecode;
 
+/*
+    方法的静态分派
+
+    Grandpa g1 = new Father();
+
+    以上代码 g1 的静态类型时Grandpa，而g1的实际类型（真正指向的类型）是Father
+
+    结论：变量的静态类型是不会发生变化的，而变量的实际类型则是可以发生变化的（多态的一种体现），实际类型时运行期方可确定。
+
+ */
 public class Test5 {
+    // 方法重载 是一种静态行为，在编译器就可以确定
     public void test(Grandpa grandpa){
         System.out.println("Grandpa");
     }
@@ -12,7 +23,7 @@ public class Test5 {
     }
 
     public static void main(String[] args) {
-        Grandpa g1 = new Grandpa();
+        Grandpa g1 = new Father();
         Grandpa g2 = new Son();
 
         Test5 t = new Test5();
