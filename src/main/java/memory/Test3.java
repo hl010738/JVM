d6f9c46e60578e5bd8144914fa8afaf0a953ebbf
talent_mbp2018 <hl010738@126.com>
2019-06-09 19:11:47 +0800

    jvisualvm 工具 和 jsonsole 工具 查看死锁

---
+++ src/main/java/memory/Test3.java
@@ -0,0 +1,37 @@
+package memory;
+
+/*
+    jvisualvm 工具 和 jsonsole 工具 查看死锁
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        new Thread(() -> A3.test(), "Thread A3").start();
+        new Thread(() -> B3.test(), "Thread B3").start();
+    }
+}
+
+class A3{
+    public static synchronized void test(){
+        System.out.println("From A3");
+
+        try {
+            Thread.sleep(3000);
+            B3.test();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class B3{
+    public static synchronized void test(){
+        System.out.println("From B3");
+
+        try {
+            Thread.sleep(3000);
+            A3.test();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
